f0a49191b69a362fbb77ee78fde7ad3b64f3910c
VRX <76213182+vrx-666@users.noreply.github.com>
2023-07-14 13:48:50 +0200

    Update list.sh
    Multiple datasets/paths in one script

--- list.sh
+++ list.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-source_dataset="darkmatter_disks/domains"
+source_dataset=( "dark_matter/appdata" "dark_matter/domains" )
 mount_point="/mnt"
 
-# Add the mount point to the dataset
-dataset_path="${mount_point}/${source_dataset}"
-
 # Get ZFS datasets
-zfs_datasets=$(zfs list -Ho name -r "$source_dataset" | awk -F '/' '{print $NF}' | tail -n +2)
+zfs_datasets=()
+for dataset in ${source_dataset[@]}; do
+    zfs_datasets+=("$(zfs list -Ho name -r "$mount_point/$dataset" | tail -n +2)")
+done
 
 # List datasets
 echo -e "\n**Datasets are**"
-if [[ -z $zfs_datasets ]]; then
-    echo "There are no datasets"
-else
-    echo "$zfs_datasets"
-fi
+echo "${zfs_datasets[@]}"
 
 # Get all directories
 all_directories=()
 oldIFS=$IFS
 IFS=$'\n'
-for dir in $(find "$dataset_path" -maxdepth 1 -mindepth 1 -type d); do
+allpaths=()
+for dataset in ${source_dataset[@]}; do
+    allpaths+=("$(find $mount_point/$dataset  -maxdepth 1 -mindepth 1 -type d)")
+done
+for dir in ${allpaths[@]}; do
     # restore the IFS
     IFS=$oldIFS
-    all_directories+=("${dir#$dataset_path/}")
+    all_directories+=("${dir#$mount_point/}")
     # change IFS back to handle newline only for the next loop iteration
     IFS=$'\n'
 done
@@ -35,7 +35,7 @@ IFS=$oldIFS
 echo -e "\n**Folders are**"
 folders_found=0
 for folder in "${all_directories[@]}"; do
-    if ! grep -Fxq "$folder" <<< "$zfs_datasets"; then
+    if ! grep -Fxq "$folder" <<< "${zfs_datasets[@]}"; then
         echo "$folder"
         folders_found=1
     fi
